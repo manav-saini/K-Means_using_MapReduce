bd37bff734eeaf8ff6370e18df768064526efc08
Manav Saini <manav20518@iiitd.ac.in>
2024-04-17 00:34:52 +0530

    logging

--- master.py
+++ master.py
@@ -6,17 +6,24 @@ import random
 import mapreduce_pb2
 import mapreduce_pb2_grpc
 import concurrent.futures
+import logging
+import time
+
+# Set up logging
+logging.basicConfig(filename='dump.txt', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 class MasterServicer(mapreduce_pb2_grpc.MapReduceServiceServicer):
-    def __init__(self, num_mappers, num_reducers, data_source, num_centroids, iterations, scenario,base_port=5000):
+    def __init__(self, num_mappers, num_reducers, data_source, num_centroids, iterations, scenario, base_port=5000):
         self.num_mappers = num_mappers
         self.num_reducers = num_reducers
-        self.data_source = data_source  
+        self.data_source = data_source
         self.num_centroids = num_centroids
         self.iterations = iterations
         self.scenario = scenario
         self.centroids = self.initialize_centroids()
         self.base_port = base_port
+        print(f"Initial centroids: {self.centroids}")
+        logging.info("Initial centroids: %s", self.centroids)
 
     def initialize_centroids(self):
         data_points = self.load_data()
@@ -34,7 +41,8 @@ class MasterServicer(mapreduce_pb2_grpc.MapReduceServiceServicer):
             data = np.concatenate([np.loadtxt(f, delimiter=',') for f in self.data_source])
         return data
 
-    def map_reduce_cycle(self):
+    def map_reduce_cycle(self, iteration):
+        print(f"Starting iteration {iteration}")
         data_points = self.load_data()
         if self.scenario == 1:
             indices = np.array_split(range(len(data_points)), self.num_mappers)
@@ -43,46 +51,83 @@ class MasterServicer(mapreduce_pb2_grpc.MapReduceServiceServicer):
             chunks = np.array_split(data_points, self.num_mappers)
         mapped = []
         reduced = []
+
         def map_chunk(chunk, mapper_id):
             port = self.base_port + mapper_id
             channel = grpc.insecure_channel(f'localhost:{port}')
             mapper_stub = mapreduce_pb2_grpc.MapReduceServiceStub(channel)
             point_messages = [mapreduce_pb2.Point(x=point[0], y=point[1]) for point in chunk]
             centroid_messages = [mapreduce_pb2.Centroid(id=i, location=mapreduce_pb2.Point(x=centroid[0], y=centroid[1])) for i, centroid in enumerate(self.centroids)]
-            try:
-                response = mapper_stub.Map(mapreduce_pb2.MapRequest(data=point_messages, centroids=centroid_messages))
-                return response.assignments
-            except grpc.RpcError as e:
-                print(f"Error communicating with mapper {mapper_id} on port {port}: {str(e)}")
-                return []
-            
+
+            # Simulated probabilistic failure
+            if random.random() < 0.5:
+                print(f"Mapper {mapper_id} call FAILED due to simulated error")
+                logging.info(f"Iteration {iteration}: Mapper {mapper_id} simulated failure")
+                time.sleep(2)  # To allow time for potential manual interruption
+                return None  # Simulate failure
+
+            response = mapper_stub.Map(mapreduce_pb2.MapRequest(data=point_messages, centroids=centroid_messages))
+            print(f"Mapper {mapper_id} call SUCCESS")
+            logging.info("Iteration %d: Mapper %d response: SUCCESS", iteration, mapper_id)
+            return response.assignments
+
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_mappers) as executor:
-            future_to_chunk = {executor.submit(map_chunk, chunk, i+1): chunk for i, chunk in enumerate(chunks)}
-            for future in concurrent.futures.as_completed(future_to_chunk):
-                chunk_result = future.result()
-                mapped.extend(chunk_result)
+            futures_to_chunk = {executor.submit(map_chunk, chunk, i+1): i+1 for i, chunk in enumerate(chunks)}
+            while futures_to_chunk:
+                for future in concurrent.futures.as_completed(futures_to_chunk):
+                    mapper_id = futures_to_chunk.pop(future)
+                    try:
+                        chunk_result = future.result()
+                        if chunk_result is None:  # Failure simulation or other error
+                            print(f"Retrying mapper {mapper_id} due to failure")
+                            logging.error(f"Iteration {iteration}: Mapper {mapper_id} retrying")
+                            futures_to_chunk[executor.submit(map_chunk, chunks[mapper_id-1], mapper_id)] = mapper_id
+                        else:
+                            mapped.extend(chunk_result)
+                    except Exception as e:
+                        print(f"Exception during mapper {mapper_id} execution, retrying. Error: {str(e)}")
+                        logging.error(f"Iteration {iteration}: Mapper {mapper_id} error {str(e)} - retrying")
+                        futures_to_chunk[executor.submit(map_chunk, chunks[mapper_id-1], mapper_id)] = mapper_id
 
         assignments = np.array_split(mapped, self.num_reducers)
 
-        print("mapping done")
-
         def reducer_assignments(assignment_group, reducer_id):
-            channel = grpc.insecure_channel(f'localhost:{self.base_port+50+ reducer_id}')
+            port = self.base_port + 50 + reducer_id
+            channel = grpc.insecure_channel(f'localhost:{port}')
             reducer_stub = mapreduce_pb2_grpc.MapReduceServiceStub(channel)
-            try:
-                response = reducer_stub.Reduce(mapreduce_pb2.ReduceRequest(reducerId=reducer_id))
-                return response.centroids
-            except grpc.RpcError as e:
-                print(f"Error communicating with reducer {reducer_id}: {str(e)}")
-                return []
-            
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_reducers) as executor:  
-            future_to_assignment_group = {executor.submit(reducer_assignments, assignment_group, i+1): assignment_group for i, assignment_group in enumerate(assignments)}
-            for future in concurrent.futures.as_completed(future_to_assignment_group):
-                assignment_group_result = future.result()
-                reduced.extend(assignment_group_result)
+
+            # Simulated probabilistic failure
+            if random.random() < 0.5:
+                print(f"Reducer {reducer_id} call FAILED due to simulated error")
+                logging.info(f"Iteration {iteration}: Reducer {reducer_id} simulated failure")
+                time.sleep(1)  # To allow time for potential manual interruption
+                return None  # Simulate failure
+
+            response = reducer_stub.Reduce(mapreduce_pb2.ReduceRequest(reducerId=reducer_id))
+            print(f"Reducer {reducer_id} call SUCCESS")
+            logging.info("Iteration %d: Reducer %d response: SUCCESS", iteration, reducer_id)
+            return response.centroids
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_reducers) as executor:
+            futures_to_assignment_group = {executor.submit(reducer_assignments, assignment_group, i+1): i+1 for i, assignment_group in enumerate(assignments)}
+            while futures_to_assignment_group:
+                for future in concurrent.futures.as_completed(futures_to_assignment_group):
+                    reducer_id = futures_to_assignment_group.pop(future)
+                    try:
+                        assignment_group_result = future.result()
+                        if assignment_group_result is None:  # Failure simulation or other error
+                            print(f"Retrying reducer {reducer_id} due to failure")
+                            logging.error(f"Iteration {iteration}: Reducer {reducer_id} retrying")
+                            futures_to_assignment_group[executor.submit(reducer_assignments, assignments[reducer_id-1], reducer_id)] = reducer_id
+                        else:
+                            reduced.extend(assignment_group_result)
+                    except Exception as e:
+                        print(f"Exception during reducer {reducer_id} execution, retrying. Error: {str(e)}")
+                        logging.error(f"Iteration {iteration}: Reducer {reducer_id} error {str(e)} - retrying")
+                        futures_to_assignment_group[executor.submit(reducer_assignments, assignments[reducer_id-1], reducer_id)] = reducer_id
 
         return reduced
+    
     def convert_new_centroids(self,protobuf_centroids):
         new_centroids = []
         for centroid in protobuf_centroids:
@@ -94,14 +139,17 @@ class MasterServicer(mapreduce_pb2_grpc.MapReduceServiceServicer):
     def run(self):
         old_centroids = np.array(self.centroids)
         for i in range(self.iterations):
-            new_centroids = self.map_reduce_cycle()
+            new_centroids = self.map_reduce_cycle(i+1)
             new_centroids = self.convert_new_centroids(new_centroids)
-            print(new_centroids)
+            print(f"Iteration {i+1}: New centroids: {new_centroids}")
+            logging.info("Iteration %d: New centroids: %s", i+1, new_centroids)
             if np.allclose(old_centroids, new_centroids, atol=1e-4):
                 print("Convergence reached.")
+                logging.info("Convergence reached after %d iterations.", i+1)
                 break
             old_centroids = new_centroids
         print("Final centroids:", new_centroids)
+        logging.info("Final centroids: %s", new_centroids)
 
 if __name__ == "__main__":
     num_mappers = int(sys.argv[1])
